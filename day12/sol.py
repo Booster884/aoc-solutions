faa51ffe8838a110f1dfd7ed14da69fd8a0fc84c
Sebastian Gielens <sgielens@icloud.com>
2022-12-12 08:51:01 +0100

    Solve day 12

--- day12/sol.py
+++ day12/sol.py
@@ -5,8 +5,6 @@ file = open("in", "r")
 dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 grid = [[*l] for l in file.read().split('\n')[:-1]]
-dirgrid = [[' ' for _ in row] for row in grid]
-# print(grid)
 
 w = len(grid[0])
 h = len(grid)
@@ -18,64 +16,61 @@ for i, row in enumerate(grid):
         spos = (row.index('S'), i)
     if 'E' in row:
         epos = (row.index('E'), i)
-curr_height = 'S'
 
-visited = set()
-frontier = [spos]
+def dist_to_end(spos, epos):
+    dirgrid = [[' ' for _ in row] for row in grid]
+    curr_height = grid[spos[1]][spos[0]]
+    visited = set()
+    frontier = [spos]
 
-i = 0
-while curr_height != 'E' and len(frontier) != 0:
-    pos = frontier.pop(0)
-    # visited.add(pos)
-    # print(pos)
-    if not(0 <= pos[0] < w) or not(0 <= pos[1] < h):
-        continue
-    curr_height = grid[pos[1]][pos[0]]
-
-    for d in dirs:
-        dpos = (pos[0] + d[0], pos[1] + d[1])
-        if not(0 <= dpos[0] < w) or not(0 <= dpos[1] < h):
+    while curr_height != 'E' and len(frontier) != 0:
+        pos = frontier.pop(0)
+        if not(0 <= pos[0] < w) or not(0 <= pos[1] < h):
             continue
+        curr_height = grid[pos[1]][pos[0]]
 
-        if dpos in visited:
-            continue
+        for d in dirs:
+            dpos = (pos[0] + d[0], pos[1] + d[1])
+            if not(0 <= dpos[0] < w) or not(0 <= dpos[1] < h):
+                continue
 
-        dhgt = grid[dpos[1]][dpos[0]]
-        if not dhgt in heights:
-            continue
-        if not curr_height in heights:
-            continue
+            if dpos in visited:
+                continue
 
-        if heights.index(dhgt) - heights.index(curr_height) <= 1:
-            dirgrid[dpos[1]][dpos[0]] = str(dirs.index(d))
-            # pos = dpos
-            # curr_height = dhgt
-            frontier.append(dpos)
-            visited.add(dpos)
-            # break
+            dhgt = grid[dpos[1]][dpos[0]]
+            if not dhgt in heights:
+                continue
+            if not curr_height in heights:
+                continue
 
-    # i += 1
-    # if i > 10:
-    #     break
+            if heights.index(dhgt) - heights.index(curr_height) <= 1:
+                dirgrid[dpos[1]][dpos[0]] = str(dirs.index(d))
+                frontier.append(dpos)
+                visited.add(dpos)
 
-# Trace back
-pos = epos
+    # Trace back
+    pos = epos
 
-for row in dirgrid:
-    print(''.join(row))
+    s = 0
+    while pos != spos:
+        t = dirgrid[pos[1]][pos[0]]
+        if t == ' ':
+            return 8320498242039820538957
+        d = dirs[int(t)]
+        pos = (pos[0] - d[0], pos[1] - d[1])
 
-s = 0
-while pos != spos:
-    print(pos)
-    t = dirgrid[pos[1]][pos[0]]
-    d = dirs[int(t)]
-    pos = (pos[0] - d[0], pos[1] - d[1])
+        s += 1
+    return s
 
-    s += 1
+# Simply traversing from E to the nearest a would have been quicker, 
+# but jank is good!
+dists = []
+for row in range(h):
+    for col in range(w):
+        tile = grid[row][col]
+        if tile == 'a':
+            _spos = (col, row)
+            dists.append(dist_to_end(_spos, epos))
 
-    i += 1
-    if i > 1000:
-        break
-
-print("1:", s)
-print("2:")
+print("1:", dist_to_end(spos, epos))
+print("2:", min(dists))
