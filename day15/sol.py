d872a9c0891656836d95ba02c48a47ab911e2e91
Sebastian Gielens <sgielens@icloud.com>
2022-12-15 11:39:36 +0100

    Solve day 15 part 2

--- day15/sol.py
+++ day15/sol.py
@@ -1,29 +1,41 @@
 import re
 file = open("in", "r")
-qy = 2000000
-# qy = 10
+area = 4000000
 
-dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+def simplify(arr):
+    new_arr = []
+    s, e = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i][0] <= e:
+            e = max(e, arr[i][1])
+        else:
+            new_arr.append((s, e))
+            s, e = arr[i]
+
+    new_arr.append((s, e))
+    return new_arr
 
 data = file.read()
-# grid = [[*l] for l in data.split('\n')[:-1]]
-
-s = 0
-l = []
-
-blocked = set()
-beacons = set()
-
+scanners = []
 p = re.compile(r"-?\d+")
+
 for line in data.split('\n')[:-1]:
     sx, sy, bx, by = [int(n) for n in p.findall(line)]
     r = abs(sx - bx) + abs(sy - by)
-    r = r - abs(qy - sy)
-    for x in range(sx - r, sx + r + 1):
-        blocked.add(x)
-    if by == qy:
-        beacons.add(bx)
+    scanners.append((sx, sy, r))
 
-print("1:", len(blocked) - len(beacons))
-print("2:")
-# 5294935 high
+for y in range(area):
+    inters = []
+    for s in scanners:
+        dy = abs(y - s[1])
+        r = s[2] - dy
+        if r > 0:
+            inters.append((s[0] - r, s[0] + r))
+    inters.sort(key=lambda x: x[0])
+    inters = simplify(inters)
+    if len(inters) == 2 and (inters[1][0] - inters[0][1] == 2):
+        x = inters[0][1] + 1
+        print(x * 4000000 + y)
+        quit()
+
+
