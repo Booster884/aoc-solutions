17a9a4dfda506faad9cbe4d21f2364309d52e919
Sebastian Gielens <sgielens@icloud.com>
2023-12-03 12:20:35 +0100

    Solve day 3 part 2
    I didn't solve this part as quickly as the git history says, and I actually also used this implementation to solve part 1.
    The previous commit is mostly to show off my first line of thinking that was only wrong because I wasn't replacing '\n' with '.'.

--- 2023/day3/sol.py
+++ 2023/day3/sol.py
@@ -1,32 +1,33 @@
 with open('input') as f:
     grid = [line.replace('\n', '.') for line in f.readlines()]
 
-numbers = []
+dirs = [(-1, 0), (0, -1), (1, 0), (0, 1), (-1, -1), (1, -1), (1, 1), (-1, 1)]
+ans1 = 0
+ans2 = 0
 
-curr = ''
+def find_adj_nums(x, y):
+    nums = set()
+    for dir_ in dirs:
+        dx, dy = dir_
+        try:
+            if grid[y + dy][x + dx].isdigit():
+                start = end = x + dx
+                while grid[y + dy][start].isdigit():
+                    start -= 1
+                while grid[y + dy][end].isdigit():
+                    end += 1
+                nums.add(int(grid[y + dy][start+1:end]))
+        except IndexError:
+            pass
+    return nums
+
 for y, line in enumerate(grid):
     for x, char in enumerate(line):
-        if char.isdigit():
-            curr += char
-        elif curr != '':
-            pos = (x - len(curr), y)
-            numbers.append((pos, curr))
-            curr = ''
-
-ans1 = 0
-
-for num in numbers:
-    pos, curr = num
-    x, y = pos
-
-    is_symbol_adj = []
-    for i in range(y - 1, y + 2):
-        for j in range(x - 1, x + len(curr) + 1):
-            try:
-                is_symbol_adj.append(grid[i][j] not in '1234567890.')
-            except IndexError:
-                pass
-    if any(is_symbol_adj):
-        ans1 += int(curr)
+        if char not in '.1234567890':
+            adj = list(find_adj_nums(x, y))
+            ans1 += sum(adj)
+            if char == '*' and len(adj) == 2:
+                ans2 += adj[0] * adj[1]
 
 print('1:', ans1)
+print('2:', ans2)
