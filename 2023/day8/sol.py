d98df957eb47274bffb9c66f0b7c0166989fb922
Sebastian Gielens <sgielens@icloud.com>
2023-12-08 09:16:54 +0100

    Solve day 8 part 2

--- 2023/day8/sol.py
+++ 2023/day8/sol.py
@@ -1,3 +1,6 @@
+from math import lcm
+
+
 with open('in') as f:
     route, nodes_ = f.read().split('\n\n')
 
@@ -7,12 +10,18 @@ for node in nodes_.strip().split('\n'):
     left, right = edges[1:-1].split(', ')
     nodes[node] = (left, right)
 
-curr = 'AAA'
+curr = [node for node in nodes.keys() if node[-1] == 'A']
+loop_lengths = [0] * len(curr)
+
 i = 0
 while True:
-    curr = nodes[curr][route[i % len(route)] == 'R']
-    if curr == 'ZZZ':
+    for j, c in enumerate(curr):
+        curr[j] = nodes[c][route[i % len(route)] == 'R']
+        if curr[j][-1] == 'Z':
+            loop_lengths[j] = i + 1
+    if all([l != 0 for l in loop_lengths]):
         break
+
     i += 1
 
-print('1:', i + 1)
+print('2:', lcm(*loop_lengths))
