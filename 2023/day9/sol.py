cae46b299d7b8a3c6ec9ff86d9208aad35693a2f
Sebastian Gielens <sgielens@icloud.com>
2023-12-09 10:36:24 +0100

    Solve day 9 part 2
    These solutions do look quite bad, though.

--- 2023/day9/sol.py
+++ 2023/day9/sol.py
@@ -9,13 +9,13 @@ for seq in sequences:
     ll = [seq]
     while not all(i == 0 for i in ll[-1]) and len(ll[-1]) > 0:
         ll.append(list(map(lambda t: t[1] - t[0], pairwise(ll[-1]))))
-    ll = list(reversed(ll))
+    ll = list(reversed([list(reversed(l)) for l in ll]))
 
     for i, l in enumerate(ll):
         if i == 0:
             ll[i].append(0)
         else:
-            ll[i].append(l[-1] + ll[i - 1][-1])
+            ll[i].append(l[-1] - ll[i - 1][-1])
     ans1.append(ll[-1][-1])
 
-print('1:', sum(ans1))
+print('2:', sum(ans1))
