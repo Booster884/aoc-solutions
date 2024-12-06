511a63f1db4a9c65f9d99faf7effa54e9e9de80d
Sebastian Gielens <sgielens@icloud.com>
2024-12-06 13:48:57 +0100

    Solve day 6 part 2

--- 2024/day06/sol.hs
+++ 2024/day06/sol.hs
@@ -3,7 +3,7 @@ import qualified Data.Set as Set
 import Data.List (transpose)
 
 type Point = (Int, Int)
-type GridState = (Set Point, Point, Point, Point, Set Point)
+type GridState = (Set Point, Point, Point, Point, Set Point, Set (Point, Point))
 
 findPosOf :: Char -> [String] -> [(Int, Int)]
 findPosOf char grid = [(i, j) | i <- [0..length grid - 1], j <- [0..length (head grid) - 1], grid !! i !! j == char]
@@ -19,6 +19,7 @@ newState grid = (fromList $ obstructions grid,
                  (length grid -1 ,length (head grid) - 1),
                  guardPos grid,
                  (0, -1),
+                 Set.empty,
                  Set.empty)
 
 rotate :: Point -> Point
@@ -27,26 +28,29 @@ rotate (0, -1) = (1, 0)
 rotate (1, 0) = (0, 1)
 rotate (0, 1) = (-1, 0)
 
-move :: GridState -> GridState
-move (obs, (w, h), (x, y), (dx, dy), been)
-    | obstructed = move (obs, (w, h), (x, y), rotate (dx, dy), newBeen)
-    | outOfBounds = (obs, (w, h), (x, y), (dx, dy), newBeen)
-    | otherwise = move (obs, (w, h), (nx, ny), (dx, dy), newBeen)
+move :: GridState -> (Set Point, Bool)
+move (obs, (w, h), (x, y), (dx, dy), been, dirBeen)
+    | obstructed = move (obs, (w, h), (x, y), rotate (dx, dy), newBeen, newDirBeen)
+    | outOfBounds = (newBeen, False)
+    | loop = (newBeen, True)
+    | otherwise = move (obs, (w, h), (nx, ny), (dx, dy), newBeen, newDirBeen)
     where (nx, ny) = (x + dx, y + dy)
           obstructed = Set.member (nx, ny) obs
           outOfBounds = not (nx `elem` [0..w] && ny `elem` [0..h])
+          loop = Set.member ((x, y), (dx, dy)) dirBeen
           newBeen = Set.insert (x, y) been
+          newDirBeen = Set.insert ((x, y), (dx, dy)) dirBeen
 
-solve1 :: GridState -> Int
-solve1 gs = Set.size been
-    where (_, _, _, _, been) = move gs
-
-solve2 = undefined
+-- Slow as hell
+solve2 :: GridState -> Set Point -> Int
+solve2 (obs, size, pos, dir, _, _) been = length $ filter snd $
+    [move (Set.insert x obs, size, pos, dir, Set.empty, Set.empty)
+     | x <- Set.toList been, x /= pos]
 
 main :: IO ()
 main =
     do inLines <- lines <$> readFile "input"
        let startState = newState $ transpose inLines
-       print $ solve1 startState
-       -- 5238 too low
-       -- print $ solve2 inLines
+       let (been, False) = move startState
+       print $ Set.size been
+       print $ solve2 startState been
