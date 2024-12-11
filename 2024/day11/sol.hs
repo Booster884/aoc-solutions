c842c80b9e384997b19f64c9fb8562c4fe625c85
Sebastian Gielens <sgielens@icloud.com>
2024-12-11 14:48:59 +0100

    Solve day 11 part 2

--- 2024/day11/sol.hs
+++ 2024/day11/sol.hs
@@ -1,6 +1,9 @@
-import Data.List (splitAt)
+{-# LANGUAGE TupleSections #-}
+import Data.List (splitAt, partition)
 import Data.Function (on)
 import Data.Ratio ((%))
+
+type IntOccur = (Int, Int)
 
 mapPair f = uncurry ((,) `on` f)
 
@@ -11,17 +14,23 @@ splitDigits :: Int -> (Int, Int)
 splitDigits x = mapPair read $ splitAt (floor (length w % 2)) w
     where w = show x
 
-iter :: [Int] -> [Int]
-iter (x:xs) | x == 0 = 1 : iter xs
-            | evenDigitCount x = let (a, b) = splitDigits x in a : b : iter xs
-            | otherwise = x * 2024 : iter xs
+iter :: [IntOccur] -> [IntOccur]
+iter ((x, n):xs) | x == 0 = (1, n) : iter xs
+                 | evenDigitCount x = let (a, b) = splitDigits x in (a, n) : (b, n) : iter xs
+                 | otherwise = (x * 2024, n) : iter xs
 iter [] = []
 
-solve :: Int -> [Int] -> Int
-solve n = length . last . take (n+1) . iterate iter
+simplify :: [IntOccur] -> [IntOccur]
+simplify ((x, n):xs) = (x, n + sum (map snd sameVal)) : simplify otherVal
+    where (sameVal, otherVal) = partition (\y -> x == fst y) xs
+simplify [] = []
+
+solve :: Int -> [IntOccur] -> Int
+solve n = sum . map snd . last . take (n+1) . iterate (simplify . iter)
 
 main :: IO ()
 main =
     do numbers :: [Int] <- map read . words <$> readFile "input"
-       print $ solve 25 numbers
-       -- print $ solve 75 numbers
+       let numOccur = map (, 1) numbers
+       print $ solve 25 numOccur
+       print $ solve 75 numOccur
