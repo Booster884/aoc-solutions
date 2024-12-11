9915e32bab7272b8f5681bc6fdb454e300e338a9
Sebastian Gielens <sgielens@icloud.com>
2024-12-11 19:55:15 +0100

    Improve day 11
    Total runtime went from ~20s to ~0.01s.

--- 2024/day11/sol.hs
+++ 2024/day11/sol.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE TupleSections #-}
 import Data.List (splitAt, partition)
 import Data.Function (on)
 import Data.Ratio ((%))
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 
-type IntOccur = (Int, Int)
+data BlinkRes = Only Int | Both Int Int
+    deriving (Show)
+type IntOccur = Map Int Int
 
 mapPair f = uncurry ((,) `on` f)
 
@@ -14,23 +17,25 @@ splitDigits :: Int -> (Int, Int)
 splitDigits x = mapPair read $ splitAt (floor (length w % 2)) w
     where w = show x
 
-iter :: [IntOccur] -> [IntOccur]
-iter ((x, n):xs) | x == 0 = (1, n) : iter xs
-                 | evenDigitCount x = let (a, b) = splitDigits x in (a, n) : (b, n) : iter xs
-                 | otherwise = (x * 2024, n) : iter xs
-iter [] = []
+singleIter :: Int -> BlinkRes
+singleIter x | x == 0 = Only 1
+             | evenDigitCount x = let (a, b) = splitDigits x in Both a b
+             | otherwise = Only (x * 2024)
 
-simplify :: [IntOccur] -> [IntOccur]
-simplify ((x, n):xs) = (x, n + sum (map snd sameVal)) : simplify otherVal
-    where (sameVal, otherVal) = partition (\y -> x == fst y) xs
-simplify [] = []
+addIter :: Int -> Int -> IntOccur -> IntOccur
+addIter n x xs = case singleIter x of
+    Only y -> Map.insertWith (+) y n xs
+    Both a b -> Map.insertWith (+) a n $ Map.insertWith (+) b n xs
 
-solve :: Int -> [IntOccur] -> Int
-solve n = sum . map snd . last . take (n+1) . iterate (simplify . iter)
+iter :: IntOccur -> IntOccur
+iter = Map.foldrWithKey (flip addIter) Map.empty
+
+solve :: Int -> IntOccur -> Int
+solve n = sum . map snd . Map.toList . last . take (n+1) . iterate iter
 
 main :: IO ()
 main =
-    do numbers :: [Int] <- map read . words <$> readFile "input"
-       let numOccur = map (, 1) numbers
+    do numbers :: [Int] <- map read . words <$> readFile "test"
+       let numOccur :: IntOccur = Map.fromList $ map (, 1) numbers
        print $ solve 25 numOccur
        print $ solve 75 numOccur
