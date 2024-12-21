8614c5471ee5dc178ed098bc252ea5e3a4614a3b
Sebastian Gielens <sgielens@icloud.com>
2024-12-21 17:28:34 +0100

    Solve day 21 part 2

--- 2024/day21/sol.py
+++ 2024/day21/sol.py
@@ -1,5 +1,7 @@
 from collections import deque
-from functools import reduce
+from functools import cache, reduce
+from itertools import pairwise, product
+from math import inf
 
 dirs = {
     ">": (1, 0),
@@ -25,7 +27,7 @@ def flatten_list(l):
     return reduce(lambda a, b: a + b, l)
 
 
-def find_path(start, end, movable):
+def find_paths(start, end, movable):
     todo = deque([(start, "")])
     seen = set()
     paths = []
@@ -55,7 +57,7 @@ def paths(sequence, positions):
     parts = [""]
     for c in sequence:
         goal = positions[c]
-        paths = find_path(curr, goal, movable)
+        paths = find_paths(curr, goal, movable)
         parts_ = []
         for path in paths:
             parts_ += [part + path for part in parts]
@@ -64,17 +66,38 @@ def paths(sequence, positions):
 
     return parts
 
+
+@cache
+def lowest_path_cost(sequence: str, level: int) -> int:
+    if level == 0:
+        return len(sequence)
+    path = 0
+    for a, b in pairwise("A" + sequence):
+        options = best_dir_moves[(a, b)]
+        paths = [lowest_path_cost(opt, level - 1) for opt in options]
+        path += sorted(paths)[0]
+    return path
+
+
+best_dir_moves = {}
+
+for start, end in product(dir_positions, repeat=2):
+    p = find_paths(dir_positions[start], dir_positions[end], dir_positions.values())
+    best_dir_moves[(start, end)] = p
+
 lines = open("input").read().splitlines()
 
-part1 = 0
+def solve(iterations):
+    sol = 0
 
-for line in lines:
-    p = paths(line, num_positions)
-    for _ in range(2):
-        best_len = min(len(x) for x in p)
-        best_p = [x for x in p if len(x) == best_len]
-        p_ = flatten_list([paths(xs, dir_positions) for xs in best_p])
-        p = p_
-    best_len = min(len(x) for x in p)
-    part1 += int(line[:-1]) * best_len
-print(part1)
+    for line in lines:
+        best = inf
+        p = paths(line, num_positions)
+        for p_ in p:
+            best = min(lowest_path_cost(p_, iterations), best)
+        sol += int(line[:-1]) * best
+
+    return sol
+
+print(solve(2))
+print(solve(25))
