19ca646f19a73ec189428b9ac885bd12fee2f09f
Sebastian Gielens <sgielens@icloud.com>
2025-12-04 20:36:00 +0100

    Improve day 2
    Generate invalid numbers instead of looping over entire range and
    filtering, and uses decimal maths to get parts of numbers.
    Time went from 1.2s to 8.5ms.

--- 2025/day02/sol.hs
+++ 2025/day02/sol.hs
@@ -1,5 +1,6 @@
 import Data.Char (isDigit)
-import Debug.Trace (trace)
+import Data.List (nub, (\\))
+import Data.Maybe (fromMaybe)
 import Text.ParserCombinators.ReadP
 
 testInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
@@ -7,35 +8,46 @@ testInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1
 intP :: ReadP Int
 intP = read <$> many1 (satisfy isDigit)
 
--- Something like `rangeP = (,) <$> intP <* char '-' *> intP` might be better?
 rangeP :: ReadP (Int, Int)
-rangeP = do
-  a <- intP
-  char '-'
-  b <- intP
-  return (a, b)
+rangeP = (,) <$> intP <*> (char '-' *> intP)
 
 parse :: ReadP [(Int, Int)]
 parse = sepBy rangeP (char ',')
 
--- | Check if a string is made of 'n' repetitions of the same substring.
-hasNRepeats :: Int -> String -> Bool
-hasNRepeats n xs
-  | length xs `mod` n == 0 = f l (take l xs) (drop l xs)
-  | otherwise = False
-  where
-    l = (length xs `div` n)
-    f _ ts [] = True
-    f l ts xs = ts == take l xs && f l ts (drop l xs)
+digits :: Int -> Int
+digits = length . show
 
-hasAnyRepeats :: String -> Bool
-hasAnyRepeats xs = any id [hasNRepeats n xs | n <- [2 .. l]]
+rep n x = f n (digits x) * x
   where
-    l = length xs
+    f 1 ls = 1
+    f n ls = 10 ^ ls * f (n - 1) ls + 1
+
+prefix' n x = 10 ^ (digits p)
+  where
+    l = digits x
+    ls = l `div` n
+    p = (x `div` 10 ^ (l - ls))
+
+prefix n x | l `mod` n == 0 = Just (x `div` 10 ^ (l - ls))
+  where
+    l = digits x
+    ls = l `div` n
+prefix _ _ = Nothing
+
+invalidInRange n (start, end) =
+  filter (>= start) $
+    takeWhile (<= end) $
+      map (rep n) [(fromMaybe (prefix' n start) $ prefix n start) ..]
+
+splitRange :: (Int, Int) -> [(Int, Int)]
+splitRange r@(start, end)
+  | digits start == digits end = [r]
+  | digits start - digits end == -1 = [(start, x - 1), (x, end)]
+  where
+    x = 10 ^ (digits start)
 
 main = do
-  -- a <- fst . last . readP_to_S parse <$> return testInput
-  a <- fst . last . readP_to_S parse <$> readFile "input"
-  print $ sum $ foldl (++) [] $ map (\(a, b) -> filter (hasNRepeats 2 . show) [a .. b]) a
-  print $ sum $ foldl (++) [] $ map (\(a, b) -> filter (hasAnyRepeats . show) [a .. b]) a
+  ranges <- concatMap splitRange . fst . last . readP_to_S parse <$> readFile "input"
 
+  print $ sum $ concat $ map (invalidInRange 2) ranges
+  print $ sum $ nub $ concat $ concatMap (\r@(s, _) -> [invalidInRange n r | n <- [2 .. digits s]]) ranges
